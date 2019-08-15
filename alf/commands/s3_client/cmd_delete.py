90c124a2aeb1b8f08d5fcd26c22c77ccdc448532
Andrew <mrhieutrieu@gmail.com>
2019-08-15 15:48:37 +0700

    add more delete function

---
+++ alf/commands/s3_client/cmd_delete.py
@@ -0,0 +1,21 @@
+import click
+import logging
+from alf import utils
+from alf.commands.s3_client.s3_client import S3Client
+
+logger = logging.getLogger(__name__)
+
+
+@click.command('delete',
+               short_help='Delete source from S3.')
+@click.option('--s3_path',
+              type=click.STRING,
+              required=False,
+              default='',
+              help='Source name of S3.')
+def delete(s3_path):
+    s3_client = S3Client(aws_access_key_id=utils.get_aws_access_key_id(),
+                         aws_secret_access_key=utils.get_aws_secret_access_key(),
+                         bucket=utils.get_aws_bucket())
+    s3_client.delete(s3_path)
+    return 0
--- alf/commands/s3_client/cmd_show.py
+++ alf/commands/s3_client/cmd_show.py
@@ -6,7 +6,7 @@ from alf.commands.s3_client.s3_client import S3Client
 logger = logging.getLogger(__name__)
 
 
-@click.command('structure',
+@click.command('show',
                short_help='Show dataset via S3.')
 @click.option('--s3_path',
               type=click.STRING,
@@ -14,19 +14,19 @@ logger = logging.getLogger(__name__)
               default='',
               help='Source name of S3.')
 @click.option('--show_file',
-              type=click.STRING,
-              required=False,
-              default='',
-              help='Show list file by the folder.')
-@click.option('--show_full_dir',
-              type=click.STRING,
+              type=click.BOOL,
               required=False,
               default=False,
+              help='Show list file by the folder.')
+@click.option('--show_full_dir',
+              type=click.BOOL,
+              required=False,
+              default=True,
               help='Show full path of the folder.')
-def structure(s3_path, show_file, show_full_dir):
+def show(s3_path, show_file, show_full_dir):
     s3_client = S3Client(aws_access_key_id=utils.get_aws_access_key_id(),
                          aws_secret_access_key=utils.get_aws_secret_access_key(),
                          bucket=utils.get_aws_bucket())
-    logger.warning("Wait a moment for display structure of '{}' pucket ...".format(utils.get_aws_bucket()))
+    logger.warning("Wait a moment for display structure of '{}' bucket ...".format(utils.get_aws_bucket()))
     s3_client.show_structure(s3_path, show_file, show_full_dir)
     return 0
