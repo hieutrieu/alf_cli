af4ac65294d8c2485046ba5a63eb4e032d3d9041
Andrew <mrhieutrieu@gmail.com>
2019-08-15 16:30:58 +0700

    Update cmd_delete.py

--- alf/commands/s3_client/cmd_delete.py
+++ alf/commands/s3_client/cmd_delete.py
@@ -1,4 +1,5 @@
 import click
+import os
 from alf import utils
 from alf.commands.s3_client.s3_client import S3Client
 
@@ -14,9 +15,10 @@ def delete(s3_path):
     s3_client = S3Client(aws_access_key_id=utils.get_aws_access_key_id(),
                          aws_secret_access_key=utils.get_aws_secret_access_key(),
                          bucket=utils.get_aws_bucket())
-    files = {}
-    files = s3_client.get_files(s3_path, files)
-    for file in files:
-        s3_client.delete(file)
-        print('Deleted "{}" file'. format(file))
+    files = s3_client.get_files(s3_path, {})
+    for dir_path, file_names in files.items():
+        for file_name in file_names:
+            s3_client.delete(os.path.join(dir_path, file_name))
+            print('Deleted "{}" file'. format(file_name))
+
     return 0
